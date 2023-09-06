c88b740c463ae5616eb1eafa223d74a82ba4a911
Shatrughan Kumar <123942835+shatrukumar47@users.noreply.github.com>
2023-09-06 23:03:45 +0530

    done

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -4,20 +4,7 @@ const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
-    password: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function (value) {
-          // Use the regex pattern for password validation
-          return /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})[a-zA-Z0-9!@#$%^&*]*$/.test(
-            value
-          );
-        },
-        message:
-          "Password must have at least one uppercase letter, one number, one special character, and be at least 8 characters long",
-      },
-    },
+    password: { type: String, required: true },
   },
   {
     versionKey: false,
