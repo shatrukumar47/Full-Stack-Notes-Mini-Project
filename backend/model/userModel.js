e2d8bb8bebcf4262ef345495e5d7f68e7381ff80
Shatrughan Kumar <123942835+shatrukumar47@users.noreply.github.com>
2023-12-13 02:11:52 +0530

    some changes

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    resetToken: { type: String, default: null },
   },
   {
     versionKey: false,
